9589f9ce64f03605533183c962a20cb3b6772071
Niklas Lindström <lindstream@gmail.com>
2013-01-23 13:33:11 +0100

    Filter out the ksamsok presentation literal

--- raa-mapper/extract_rdf.py
+++ raa-mapper/extract_rdf.py
@@ -5,12 +5,14 @@ RDF_URI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 RDF = "{%s}" % RDF_URI
 PMH = "{http://www.openarchives.org/OAI/2.0/}"
 
-def find_records(source):
+def find_records(source, ignore=()):
     context = iterparse(source, events=("start", "end"))
     root = context.next()[1]
     for event, elem in context:
         if event != 'end':
             continue
+        if elem.tag in ignore:
+            elem.clear()
         if elem.tag == PMH+"record":
             yield elem
         root.clear()
@@ -23,9 +25,10 @@ if __name__ == '__main__':
         source = gzip.open(sourcepath, 'rb')
     else:
         source = open(sourcepath)
+    ignore = ignore=["{http://kulturarvsdata.se/ksamsok#}presentation"]
     print '<RDF xmlns="%s">' % RDF_URI
     with source:
-        for record in find_records(source):
+        for record in find_records(source, ignore):
             data = record.find(PMH+"metadata")
             rdf = data.find(RDF+'RDF')
             if rdf is None:
